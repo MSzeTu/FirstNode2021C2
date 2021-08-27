118f0fde919cd93dcfd72bed4accd746fe1bf7be
Matthew Sze-Tu (RIT Student) <ms1937@rit.edu>
2021-08-27 14:13:28 -0400

    Server up

--- src/server.js
+++ src/server.js
@@ -2,18 +2,34 @@ const myData = require('./mydata.js');
 const Polygon = require('./Polygon.js');
 const _ = require('underscore');
 
-console.dir('hello');
+const http = require('http');
 
-const helloWorld = () => {
-    console.log("Hello World!");
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+    if(request.url === '/favicon.ico') {
+        console.log('favicon request');
+    }
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write(myData.getMessage());
+    response.end();
 };
 
-helloWorld();
+http.createServer(onRequest).listen(port);
 
-myData.getMessage();
+// console.dir('hello');
 
-const myPolygon = new Polygon(10, 15);
+// const helloWorld = () => {
+//     console.log("Hello World!");
+// };
 
-const array = [1,2,3,4,5];
-const found = _.contains(array, 3);
-console.log(found);
+// helloWorld();
+
+// myData.getMessage();
+
+// const myPolygon = new Polygon(10, 15);
+
+// const array = [1,2,3,4,5];
+// const found = _.contains(array, 3);
+// console.log(found);
